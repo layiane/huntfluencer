5c6d03429b25d24ca472e4235ac35fba5d5a4d11
theo2bel <theo2bel@gmail.com>
2022-11-21 23:00:31 +0100

    premiere interface

---
+++ huntfluencer/polls/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('trouver_mon_influenceur', views.trouver_mon_influenceur, name='trouver_mon_influenceur'),
+    path('contact', views.contact, name='contact'),
+]
+    
---
+++ huntfluencer/polls/views.py
@@ -0,0 +1,19 @@
+from multiprocessing import context
+from re import template
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+
+def trouver_mon_influenceur(request):
+    return render(request,'trouver_mon_influenceur.html')
+  
+
+def contact(request):
+    return render(request,'contact.html')
+   
